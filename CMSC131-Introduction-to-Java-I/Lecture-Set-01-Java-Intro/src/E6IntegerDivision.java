2f0b4979c0f67223b3554672f694b35442677bb4
emtaylor1993 <etaylorsubs1993@gmail.com>
2021-10-27 15:30:53 -0400

    + added Lecture Set 01 Java Intro code.

---
+++ CMSC131-Introduction-to-Java-I/Lecture-Set-01-Java-Intro/src/E6IntegerDivision.java
@@ -0,0 +1,11 @@
+/* CURIOUS...*/
+public class E6IntegerDivision {
+	
+	public static void main(String[] args) {
+		double x;
+		int a = 3, b =4;
+		x = a/(double)b;
+		System.out.println("Three divided by four is: " + x);
+	}
+
+}
