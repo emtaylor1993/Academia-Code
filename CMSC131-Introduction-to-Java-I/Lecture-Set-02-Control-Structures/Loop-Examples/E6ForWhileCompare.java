71665fc8ab094d4b1be642c067178e8798d84dd6
emtaylor1993 <etaylorsubs1993@gmail.com>
2021-10-27 16:49:40 -0400

    + added Lecture Set 02 Control Structures code.

---
+++ CMSC131-Introduction-to-Java-I/Lecture-Set-02-Control-Structures/Loop-Examples/E6ForWhileCompare.java
@@ -0,0 +1,42 @@
+package LoopExamples;
+
+public class E6ForWhileCompare {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		final int START = 1;
+		final int END = 10;
+
+		//counted while loop examples
+		int curr = START;
+		while (curr <= END){
+			int i = 5;
+			System.out.println("counting at : "+curr+i);
+			curr = curr + 1;
+		}
+		
+		System.out.println("I'm done; curr = "+curr);
+
+		//for loop examples
+		for (int cur = START; cur <= END; cur = cur+1){
+			System.out.println("counting at : "+cur);
+		}
+		
+		// Uncomment the next line to see the error
+		// System.out.println("I'm done; cur = "+cur);
+		
+		for (int cur = END; cur >= START; cur = cur - 1){
+			System.out.println("countint at : "+cur);
+		}
+		
+		for (int cur = 1024; cur >= 1; cur = cur /2){
+			System.out.println("counting at : "+cur);
+		}
+		
+		for (int cur = 1024; cur >=1; cur = cur - 2){
+			System.out.println("counting at : "+cur);			
+		}
+	}
+}
