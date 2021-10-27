0a4d7164f6da69465b8eac530b21635d64579c15
Emmanuel M. Taylor Jr <93065901+emtaylor1993@users.noreply.github.com>
2021-10-27 16:00:58 -0400

    * updated format

--- CMSC131-Introduction-to-Java-I/Lecture-Set-01-Java-Intro/src/E3Concatination.java
+++ CMSC131-Introduction-to-Java-I/Lecture-Set-01-Java-Intro/src/E3Concatination.java
@@ -3,20 +3,15 @@
 
 public class E3Concatination {
 	public static void main(String[] args) {
-		System.out.println("My " + "name " + 
-				"is " 
-				+ "Fred."				);
+		System.out.println("My " + "name " + "is " + "Fred.");
 		int secondsPerMinute = 60;
 		int minutesPerLecture = 50;
 		int secondsPerLecture = secondsPerMinute * minutesPerLecture;
 		
-		System.out.println("There are " + secondsPerLecture + 
-		" seconds in a lecture.");
+		System.out.println("There are " + secondsPerLecture + " seconds in a lecture.");
 		
 		System.out.print("There are ");
 		System.out.print(secondsPerLecture );
-		System.out.println(	" seconds in a lecture.");
-		
-		
+		System.out.println(" seconds in a lecture.");
 	}
 }
