39fb59289f412b72122c1a3d3c3a50682b821a14
emtaylor1993 <etaylorsubs1993@gmail.com>
2021-10-28 02:35:48 -0400

    + added code for Homework 1: Word Entry Counter.

---
+++ CMSC132-Introduction-to-Java-II/Homework-1/Index/InvalidParameterException.java
@@ -0,0 +1,6 @@
+package index;
+
+public class InvalidParameterException extends Exception {
+  private static final long serialVersionUID= 1L;  // just to avoid a Java
+                                                   // warning
+}
